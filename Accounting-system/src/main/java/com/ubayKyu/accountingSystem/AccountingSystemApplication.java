f3c4ca7675e50fafc08846efaaf23e9f435d200d
HauWen <suounoshigure@gmail.com>
2022-01-10 14:47:57 +0800

    220110 排版修正

--- Accounting-system/src/main/java/com/ubayKyu/accountingSystem/AccountingSystemApplication.java
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/AccountingSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccountingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountingSystemApplication.class, args);  //入口點
+		SpringApplication.run(AccountingSystemApplication.class, args); // 入口點
 	}
 
 }
