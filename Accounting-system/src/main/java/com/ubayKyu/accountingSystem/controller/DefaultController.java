e413ae6b3dc0549ecf4061b74acfed4fc7d828fe
HauWen <suounoshigure@gmail.com>
2022-01-10 18:28:11 +0800

    220110 移除未使用的參考

--- Accounting-system/src/main/java/com/ubayKyu/accountingSystem/controller/DefaultController.java
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/controller/DefaultController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
